8f164e9ab61426f3d00f9d66aea542e376d2d983
RodrigoRoss27 <rodrigo_rosas@connext.com.mx>
2016-06-01 13:25:47 -0500

    Este archivo contiene el esquema general de como vamos a conectarnos al equipo para simplificar el uso de expect

---
+++ ConnectDevice.py
@@ -0,0 +1,18 @@
+# This file is to define connection to device via Expect como funcion:
+#
+# ConectaEquipo(<ip>,<script>)
+#
+#
+
+def ConectaEquipo(ip_address,scripts)
+    # Verificamos si es alcanzable
+
+    # Conectamos a la DB y obtenemos usuario y contraseña correspondiente a la ip_address
+
+    # Conectamos via expect y ejecutamos el script
+
+    # Manejo de errores
+
+    # Return
+
+
