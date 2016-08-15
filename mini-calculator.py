3e1123dde34081c45369b1268681021bc244ad98
Beto Garcia <ingtiti@users.noreply.github.com>
2016-08-15 16:48:06 -0500

    mini-calculator.py initialised for DevOps Demo
    Código inicial para demostración de caso practico

---
+++ mini-calculator.py
@@ -0,0 +1,11 @@
+print("Demo para Reunion de Ingenieria")
+print("sesion: DevOps")
+
+print("\n by: moacosta / eriksala / farandaa / hegarcia \n")
+
+#asignacion de variables
+numA = int(input("Valor de A: "))
+numB = int(input("Valor de B: "))
+
+#sumatoria de numeros
+print("resultado de A + B : ", numA+numB )
