4dc9e96d2a158c80b8c89b4b66929e0daeb3bf8e
moacosta <moacosta@cisco.com>
2016-08-30 12:42:36 -0500

    Update cambio Titulo
    Cambio 1 : Cambio de titulo en el programa master / original

--- mini-calculator.py
+++ mini-calculator.py
@@ -1,5 +1,5 @@
-#Aqui se cambia el titulo
-print("Demo para Reunion de Ingenieria")
+#Aqui se cambia el titulo 
+print("Demo para Equipo de Ingenieria")
 print("sesion: DevOps")
 
 print("\n by: moacosta / hegarcia \n")
@@ -11,4 +11,6 @@ numB = int(input("Valor de B: "))
 #sumatoria de numeros
 print("resultado de A + B : ", numA+numB )
 
-#Agregar la resta
+#Agregar la resta 
+
+
