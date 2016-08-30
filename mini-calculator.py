a9f6047b3fc4d0f85b84bdcca8192894f0184b73
Beto Garcia <ingtiti@users.noreply.github.com>
2016-08-30 12:29:26 -0500

    cambios para demo
    cambios para demo

--- mini-calculator.py
+++ mini-calculator.py
@@ -1,7 +1,8 @@
+#Aqui se cambia el titulo
 print("Demo para Reunion de Ingenieria")
 print("sesion: DevOps")
 
-print("\n by: moacosta / eriksala / farandaa / hegarcia \n")
+print("\n by: moacosta / hegarcia \n")
 
 #asignacion de variables
 numA = int(input("Valor de A: "))
@@ -9,3 +10,5 @@ numB = int(input("Valor de B: "))
 
 #sumatoria de numeros
 print("resultado de A + B : ", numA+numB )
+
+#Agregar la resta
